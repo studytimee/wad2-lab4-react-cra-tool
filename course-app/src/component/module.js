0f3654e6f040e15088bcb88bd5bda4b0dadf276b
Abid Ali <studytimee@gmail.com>
2022-03-26 07:14:00 +0000

    Add module component
    Created first the last component of the hierarchy e.g App -> Course -> (Module)

---
+++ course-app/src/component/module.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Module = (props) => {
+    return (
+        <tr>
+            <td>{props.description.a}</td>
+            <td>{props.description.b}</td>
+        </tr>
+    );
+
+}
+
+export default Module
+
