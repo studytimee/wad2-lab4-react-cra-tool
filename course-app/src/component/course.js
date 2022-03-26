af733cab3333f965b246ce438bf61f010e4786e1
Abid Ali <studytimee@gmail.com>
2022-03-26 07:15:49 +0000

    Add course Component
    Created second the last component of the hierarchy e.g App -> (Course) -> Module

---
+++ course-app/src/component/course.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Module from "./module";
+
+const Course = (props) => {
+    const tData = props.modules.map((module, index) => (
+        <Module key={index} description={module} />
+    )
+    )
+    return (
+        <>
+            <h2>{`${props.course} module table`}</h2>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>No lectures</th>
+                        <th>No practicals</th>
+                    </tr>
+                </thead>
+                <tbody>{tData}</tbody>
+            </table>
+        </>
+    );
+}
+
+export default Course;
